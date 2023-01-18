b1d7933397ab4b97db458b6e746caf5fe3867db0
Neuro84x <nikeframe1984@gmail.com>
2023-01-18 19:30:35 +0700

    percent buttons

--- main.py
+++ main.py
@@ -25,18 +25,44 @@ class Application(ctk.CTk):
         self.refresh_button = ctk.CTkButton(self, text='Обновить', command=self.refresh)
         self.refresh_button.grid()
         self.refresh_label = ctk.CTkLabel(self, text="Обновление не запущено")
-        self.refresh_label.grid()
+        self.refresh_label.grid(row=0, column=0)
 
         # --------------------------------STATUS-----------------------------------------------------------------------#
-        self.bar = ctk.CTkFrame(self)
-        self.bar.grid()
+        self.bar = ctk.CTkFrame(self, width=200)
+        self.bar.grid(row=1, column=0)
 
-        self.article_status_label = ctk.CTkLabel(self.bar, text='Статус')
-        self.article_status_label.grid()
+        self.article_status_label = ctk.CTkLabel(self.bar, text='Статус', anchor='n')
+        self.article_status_label.grid(row=0, column=1)
         self.article_status = ctk.CTkComboBox(self.bar, values=['Все', 'Критично', 'Внимание', 'Четко'],
                                               state='readonly', text_color='Black', command=self.make_data_table)
         self.article_status.set('Все')
-        self.article_status.grid()
+        self.article_status.grid(row=1, column=1)
+
+        self.central_storage_label = ctk.CTkLabel(self.bar, text='ЦФО, %')
+        self.south_storage_label = ctk.CTkLabel(self.bar, text='Юг, %')
+        self.west_storage_label = ctk.CTkLabel(self.bar, text='Восток, %')
+
+        self.central_storage_label.grid(row=1, column=3)
+        self.south_storage_label.grid(row=2, column=3)
+        self.west_storage_label.grid(row=3, column=3)
+
+        self.percent_change_button = ctk.CTkButton(master=self.bar, text='Изменить проценты',
+                                                   command=self.change_percent)
+        self.percent_change_button.grid(row=0, column=2)
+
+        self.central_percent_box = ctk.CTkEntry(master=self.bar, placeholder_text='50')
+        self.central_percent_box.configure(state='readonly')
+        self.central_percent_box.insert(0, '50')
+        self.south_percent_box = ctk.CTkEntry(master=self.bar, placeholder_text='20')
+        self.south_percent_box.configure(state='readonly')
+        self.south_percent_box.insert(0, '20')
+        self.west_percent_box = ctk.CTkEntry(master=self.bar, placeholder_text='30')
+        self.west_percent_box.configure(state='readonly')
+        self.west_percent_box.insert(0, '30')
+
+        self.central_percent_box.grid(row=1, column=2)
+        self.south_percent_box.grid(row=2, column=2)
+        self.west_percent_box.grid(row=3, column=2)
 
         # ----------------------------------DATA-----------------------------------------------------------------------#
         self.sort_statuses = ('-', 'v', '^')
@@ -83,7 +109,7 @@ class Application(ctk.CTk):
 
         self.scrollbar = ttk.Scrollbar(self.data_frame, orient=ctk.VERTICAL, command=self.data.yview)
         self.data.configure(yscrollcommand=self.scrollbar.set)
-        self.scrollbar.grid(row=0, column=1, sticky='ns')
+        self.scrollbar.grid(row=0, column=10, sticky='ns')
         self.data.heading('article', text='Артикль')
         self.data.heading('amount', text='Количество')
         # self.data.heading('status', text='Статус')
@@ -94,7 +120,7 @@ class Application(ctk.CTk):
         self.data.heading('storage_west', text='Восток')
 
         self.make_data_table()
-        self.data.grid()
+        self.data.grid(row=0, column=1)
 
         # ----------------------------------SETTINGS-------------------------------------------------------------------#
 
@@ -106,11 +132,12 @@ class Application(ctk.CTk):
         self.destroy()
         sys.exit()
 
-    def make_data_table(self, event=None, internal_data_list=[]):
+    def make_data_table(self, event=None, internal_data_list=[], percent_central=50, percent_south=20, percent_west=30):
+        # print(event, percent_central, percent_west, percent_south)
         if len(internal_data_list) == 0:
             internal_data_list = self.data_list
 
-        if event in ('Все', 'Критично', 'Внимание', 'Четко'):
+        if event in ('Все', 'Критично', 'Внимание', 'Четко', 'Процент'):
             for i in self.data.get_children():
                 self.data.delete(i)
 
@@ -120,47 +147,62 @@ class Application(ctk.CTk):
                 if i[2] == 0:
                     self.data.insert("", ctk.END,
                                      values=(i[0], i[1], i[3],
-                                             math.ceil(i[3] / 100 * 50), #ЦФО
-                                             math.ceil(i[3] / 100 * 20), #ЮГ
-                                             math.ceil(i[3] / 100 * 30)  #ВОСТОК
+                                             math.ceil(i[3] / 100 * percent_central),  # ЦФО
+                                             math.ceil(i[3] / 100 * percent_south),  # ЮГ
+                                             math.ceil(i[3] / 100 * percent_west)  # ВОСТОК
                                              ),
                                      tag='red', image=self._img_red)
                 elif i[2] == 1:
                     self.data.insert("", ctk.END, values=(i[0], i[1], i[3],
-                                             math.ceil(i[3] / 100 * 50), #ЦФО
-                                             math.ceil(i[3] / 100 * 20), #ЮГ
-                                             math.ceil(i[3] / 100 * 30)  #ВОСТОК
-                                             ), tag='yellow', image=self._img_yellow)
+                                                          math.ceil(i[3] / 100 * percent_central),
+                                                          # ЦФО
+                                                          math.ceil(i[3] / 100 * percent_south),
+                                                          # ЮГ
+                                                          math.ceil(i[3] / 100 * percent_west)
+                                                          # ВОСТОК
+                                                          ), tag='yellow', image=self._img_yellow)
                 elif i[2] == 2:
                     self.data.insert("", ctk.END, values=(i[0], i[1], i[3],
-                                             math.ceil(i[3] / 100 * 50), #ЦФО
-                                             math.ceil(i[3] / 100 * 20), #ЮГ
-                                             math.ceil(i[3] / 100 * 30)  #ВОСТОК
-                                             ), tag='green', image=self._img_green)
+                                                          math.ceil(i[3] / 100 * percent_central),
+                                                          # ЦФО
+                                                          math.ceil(i[3] / 100 * percent_south),
+                                                          # ЮГ
+                                                          math.ceil(i[3] / 100 * percent_west)
+                                                          # ВОСТОК
+                                                          ), tag='green', image=self._img_green)
         elif status == 'Критично':
             for i in self.data_list:
                 if i[2] == 0:
                     self.data.insert("", ctk.END, values=(i[0], i[1], i[3],
-                                             math.ceil(i[3] / 100 * 50), #ЦФО
-                                             math.ceil(i[3] / 100 * 20), #ЮГ
-                                             math.ceil(i[3] / 100 * 30)  #ВОСТОК
-                                             ), tag='red', image=self._img_red)
+                                                          math.ceil(i[3] / 100 * percent_central),
+                                                          # ЦФО
+                                                          math.ceil(i[3] / 100 * percent_south),
+                                                          # ЮГ
+                                                          math.ceil(i[3] / 100 * percent_west)
+                                                          # ВОСТОК
+                                                          ), tag='red', image=self._img_red)
         elif status == 'Внимание':
             for i in self.data_list:
                 if i[2] == 1:
-                    self.data.insert("", ctk.END, vvalues=(i[0], i[1], i[3],
-                                             math.ceil(i[3] / 100 * 50), #ЦФО
-                                             math.ceil(i[3] / 100 * 20), #ЮГ
-                                             math.ceil(i[3] / 100 * 30)  #ВОСТОК
-                                             ), tag='yellow', image=self._img_yellow)
+                    self.data.insert("", ctk.END, values=(i[0], i[1], i[3],
+                                                          math.ceil(i[3] / 100 * percent_central),
+                                                          # ЦФО
+                                                          math.ceil(i[3] / 100 * percent_south),
+                                                          # ЮГ
+                                                          math.ceil(i[3] / 100 * percent_west)
+                                                          # ВОСТОК
+                                                          ), tag='yellow', image=self._img_yellow)
         elif status == 'Четко':
             for i in self.data_list:
                 if i[2] == 2:
                     self.data.insert("", ctk.END, values=(i[0], i[1], i[3],
-                                             math.ceil(i[3] / 100 * 50), #ЦФО
-                                             math.ceil(i[3] / 100 * 20), #ЮГ
-                                             math.ceil(i[3] / 100 * 30)  #ВОСТОК
-                                             ), tag='green', image=self._img_green)
+                                                          math.ceil(i[3] / 100 * percent_central),
+                                                          # ЦФО
+                                                          math.ceil(i[3] / 100 * percent_south),
+                                                          # ЮГ
+                                                          math.ceil(i[3] / 100 * percent_west)
+                                                          # ВОСТОК
+                                                          ), tag='green', image=self._img_green)
             # self.data.tag_configure('red', background='red')
             # self.data.tag_configure('yellow', background='yellow')
             # self.data.tag_configure('green', background='green')
@@ -202,6 +244,32 @@ class Application(ctk.CTk):
                 idl = sorted(self.data_list, key=self.sort_col, reverse=False)
                 self.active_sort_status = -1
             self.make_data_table(event=self.article_status.get(), internal_data_list=idl)
+
+    def percent_calc(self, event='Процент'):
+        if self.central_percent_box.get() == '':
+            central_per = 50
+            south_per = 20
+            west_per = 30
+        else:
+            central_per = int(self.central_percent_box.get())
+            south_per = int(self.south_percent_box.get())
+            west_per = int(self.west_percent_box.get())
+
+        self.make_data_table(event=event, percent_central=central_per, percent_west=west_per, percent_south=south_per)
+
+    def change_percent(self, event='Процент'):
+        if self.central_percent_box.cget('state') == 'readonly':
+            self.central_percent_box.configure(state='normal')
+            self.south_percent_box.configure(state='normal')
+            self.west_percent_box.configure(state='normal')
+            self.percent_change_button.configure(text='Сохранить')
+        elif self.central_percent_box.cget('state') == 'normal':
+            self.central_percent_box.configure(state='readonly')
+            self.south_percent_box.configure(state='readonly')
+            self.west_percent_box.configure(state='readonly')
+            self.percent_change_button.configure(text='Изменить проценты')
+
+        self.percent_calc(event=event)
 
 
 ctk.set_appearance_mode("dark")  # Modes: "System" (standard), "Dark", "Light"
